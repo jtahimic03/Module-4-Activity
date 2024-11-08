ee1dd80c6d353fe6c3c68e855a34ffa326d865d0
Jansen Tahimic <jtahimic@academic.rrc.ca>
2024-11-07 23:26:15 -0600

    initial commit

---
+++ contact_list/contact_list.py
@@ -0,0 +1,54 @@
+from PySide6.QtWidgets import  QMainWindow, QLineEdit, QPushButton, QTableWidget, QLabel, QVBoxLayout, QWidget, QTableWidgetItem
+
+class ContactList(QMainWindow):
+    """
+    Contact List Class (QMainWindow). Provides users a 
+    way to manage their contacts.
+    """
+    def __init__(self):
+        """
+        Initializes a Contact List window in which 
+        users can add and remove contact data.
+        """
+        super().__init__()
+        self.__initialize_widgets()      
+
+
+    def __initialize_widgets(self):
+        """
+        Given:  Code to create and initialize the QWindow
+        and all of the widgets on the window.
+        DO NOT EDIT.
+        """
+        self.setWindowTitle("Contact List")
+
+        self.contact_name_input = QLineEdit(self)
+        self.contact_name_input.setPlaceholderText("Contact Name")
+
+        self.phone_input = QLineEdit(self)
+        self.phone_input.setPlaceholderText("Phone Number")
+
+        self.add_button = QPushButton("Add Contact", self)
+        self.remove_button = QPushButton("Remove Contact", self)
+        
+        self.contact_table = QTableWidget(self)
+        self.contact_table.setColumnCount(2)
+        self.contact_table.setHorizontalHeaderLabels(["Name", "Phone"])
+
+        self.status_label = QLabel(self)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.contact_name_input)
+        layout.addWidget(self.phone_input)
+        layout.addWidget(self.add_button)
+        layout.addWidget(self.remove_button)
+        layout.addWidget(self.contact_table)
+        layout.addWidget(self.status_label)
+
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+
+
+
+
